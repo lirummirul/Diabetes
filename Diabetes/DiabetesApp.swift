c29f3f970478f015a5422c8ac38346e815c40f93
lirummirul <lada.yakovleva.2018@mail.ru>
2024-04-25 15:37:45 +0300

    init

---
+++ Diabetes/DiabetesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiabetesApp.swift
+//  Diabetes
+//
+//  Created by Lambert Lani on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DiabetesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
